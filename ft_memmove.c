5ec78739e77aa8761c0ed700b03da2172a73bcfe
Mohammed Elkholy <28461793+SolarisCode@users.noreply.github.com>
2022-06-13 22:30:03 +0200

    memmove has been modified

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: solariscode <solariscode@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/23 20:48:08 by melkholy          #+#    #+#             */
-/*   Updated: 2022/02/13 14:21:03 by solariscode      ###   ########.fr       */
+/*   Updated: 2022/06/13 22:29:49 by melkholy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,18 @@
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	size_t	count;
-	char	*str1;
-	char	*str2;
 
 	if (!dest && !src)
 		return (NULL);
-	str1 = (char *)dest;
-	str2 = (char *)src;
 	count = 0;
 	if (src < dest)
 		while (n--)
-			str1[n] = str2[n];
+			((unsigned char *)dest)[n] = ((unsigned char *)src)[n];
 	else
 	{
 		while (count < n)
 		{
-			str1[count] = str2[count];
+			((unsigned char *)dest)[count] = ((unsigned char *)src)[count];
 			count ++;
 		}
 	}
